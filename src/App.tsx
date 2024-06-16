679c1b912ed7718007aef7297cf0daaf6b7f1df6
Alirezaw <Alirezawmoradi@gmail.com>
2024-06-16 19:36:32 +0330

    styles refactored

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import {Reminder} from "./features/reminder.tsx";
 function App() {
 
     return (
-        <div className='flex min-h-screen text-gray-300 justify-center items-center bg-gray-800'>
+        <div className='flex min-h-screen text-gray-100 justify-center items-center bg-[#13111c] font-bold text-base'>
             <Reminder/>
         </div>
     )
