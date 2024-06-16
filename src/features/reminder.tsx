5b5d7cdb543938a4f1f5cc8095a11200bae69a79
Alirezaw <Alirezawmoradi@gmail.com>
2024-06-16 19:37:24 +0330

    structure created image added

--- src/features/reminder.tsx
+++ src/features/reminder.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import logo from '../../public/images/9.avif'
 
 export const Reminder: React.FC = () => {
-    return(
-        <div className='font-bold text-base'>
-            Airdrop Claim Reward
+    return (
+        <div className='flex flex-col justify-center items-center gap-2'>
+            <h1 className='text-xl tracking-widest uppercase'>Airdrop Claim Reminder</h1>
+            <img src={logo} alt='hamster'/>
+            <span>Floating action button</span>
+            <div className='flex gap-44 mt-20'>
+                <span>Logo</span>
+                <span>Timer</span>
+            </div>
         </div>
     )
 }
