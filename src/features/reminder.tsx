94e055cf7d9b9ab07ce3d8a94e068bcb0a2f4f82
Alirezaw <Alirezawmoradi@gmail.com>
2024-06-16 20:00:24 +0330

    structure created

--- src/features/reminder.tsx
+++ src/features/reminder.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 import logo from '../../public/images/9.avif'
+import hamster from '../../public/images/hamster-coin.webp'
+import {IoIosAddCircleOutline} from "react-icons/io";
+import {Timer} from "../components/timer.tsx";
 
 export const Reminder: React.FC = () => {
     return (
         <div className='flex flex-col justify-center items-center gap-2'>
-            <h1 className='text-xl tracking-widest uppercase'>Airdrop Claim Reminder</h1>
+            <h1 className='text-xl tracking-widest uppercase'>
+                Airdrop Claim Reminder
+            </h1>
+            <span className='font-light text-sm uppercase tracking-wider'>Save your Time with me</span>
             <img src={logo} alt='hamster'/>
-            <span>Floating action button</span>
-            <div className='flex gap-44 mt-20'>
-                <span>Logo</span>
-                <span>Timer</span>
+            <div className='flex flex-col justify-center items-center gap-3'>
+                <IoIosAddCircleOutline className='h-10 w-10'/>
+                <p className='font-medium text-sm'>Add</p>
+            </div>
+            <div className='flex gap-44 mt-10'>
+                <Timer/>
+                <div className='flex justify-center items-center gap-5'>
+                    <p className='font-medium text-sm'>Hamster</p>
+                    <img src={hamster} width={25} height={25} alt='hamster-coin'/>
+                </div>
             </div>
         </div>
     )
