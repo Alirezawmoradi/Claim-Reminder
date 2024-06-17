c772369d0490c5bd7a8e9ecf09bf91aed406e773
Alirezaw <Alirezawmoradi@gmail.com>
2024-06-17 04:02:32 +0330

    timer component added

--- src/components/timer.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const Timer: React.FC = () => {
-    return (
-        <p>Timer</p>
-    )
-}
---
+++ src/components/timer/timer.tsx
@@ -0,0 +1,118 @@
+// @ts-ignore
+import React, {forwardRef, useImperativeHandle} from "react";
+import {Size, TimerProps, TimerRef} from "./timer.types.ts";
+import {useTimer} from "react-timer-hook";
+import classNames from "classnames";
+import {TimerProgress} from "./timer-progress.tsx";
+
+const sizeClasses: Record<Size, string> = {
+    tiny: 'timer-xs',
+    small: 'timer-sm',
+    normal: 'timer-md',
+    large: 'timer-lg'
+};
+
+const calculateTotalSeconds = (hours: number, minutes: number, seconds: number): number => hours * 60 * 60 + minutes * 60 + seconds;
+
+export const Timer = forwardRef<TimerRef, TimerProps>(
+    (
+        {
+            expiryTimestamp,
+            autoStart,
+            onExpire,
+            size = 'tiny',
+            className,
+            showTitle = true,
+            variant = 'primary'
+        },
+        ref
+    ) => {
+        const {hours, minutes, seconds, start, pause, resume, restart} = useTimer({
+            expiryTimestamp,
+            onExpire,
+            autoStart
+        })
+        const classes = classNames(
+            'timer',
+            {[`${sizeClasses[size]}`]: size},
+            {[`timer-${variant}`]: variant},
+            className
+        )
+
+        useImperativeHandle(ref, () => (
+            {
+                start,
+                pause,
+                resume,
+                restart
+            }
+        ))
+
+        const maxHoursValue = calculateTotalSeconds(24, 0, 0);
+        const maxMinutesValue = calculateTotalSeconds(0, 60, 0);
+        const maxSecondsValue = calculateTotalSeconds(0, 0, 60);
+
+        const hoursValue = calculateTotalSeconds(hours, 0, 0);
+        const minutesValue = calculateTotalSeconds(0, minutes, 0);
+        const secondsValue = calculateTotalSeconds(0, 0, seconds);
+
+        const renderTimerProgress = (
+            unit: number,
+            value: number,
+            maxValue: number,
+            datePart: string
+        ) => {
+            if (value != null) {
+                return (
+                    <>
+                        <TimerProgress value={value}
+                                       maxValue={maxValue}
+                                       datePart={datePart}
+                                       size={size}
+                                       showTitle={showTitle}
+                                       variant={variant}
+                        >
+                            {unit}
+                        </TimerProgress>
+                    </>
+                )
+            }
+        }
+
+
+        const timeUnits = [
+            {
+                show: seconds != null,
+                unit: seconds,
+                value: secondsValue,
+                maxValue: maxSecondsValue,
+                datePart: 'seconds'
+            }, {
+                show: minutes != null,
+                unit: minutes,
+                value: minutesValue,
+                maxValue: maxMinutesValue,
+                datePart: 'minutes'
+            }, {
+                show: hours != null,
+                unit: hours,
+                value: hoursValue,
+                maxValue: maxHoursValue,
+                datePart: 'hours'
+            },
+        ]
+
+        return (
+            <div className={`${classes} flex flex-row-reverse justify-center gap-4`}>
+                {
+                    timeUnits.map(
+                        ({show, unit, value, maxValue, datePart}) => show ?
+                            renderTimerProgress(unit, value, maxValue, datePart)
+                            :
+                            null
+                    )
+                }
+            </div>
+        )
+    }
+)
