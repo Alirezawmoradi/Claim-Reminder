801d8c0bbf3b5190285645a2f5507d463ca8397d
Alirezaw <Alirezawmoradi@gmail.com>
2024-07-09 22:55:27 +0330

    local storage added to save the timer in localstorage

--- src/features/reminder.tsx
+++ src/features/reminder.tsx
@@ -1,4 +1,4 @@
-import React, {useRef} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import logo from '../../public/images/9.avif'
 import hamster from '../../public/images/hamster-coin.webp'
 import {IoIosAddCircleOutline} from "react-icons/io";
@@ -7,17 +7,36 @@ import {TimerRef} from "../components/timer/timer.types.ts";
 import {sendMessage} from "../api/send-message.ts";
 
 export const Reminder: React.FC = () => {
+
+    const savedExpireTimeStamp = localStorage.getItem('expiryTimeStamp');
+
     const getTwoMinutesFromNow = () => {
         const time = new Date();
-        time.setSeconds(time.getSeconds() + 5);
+        time.setSeconds(time.getSeconds() + 100);
         return time;
     };
+
+    const [expiryTimeStamp, setExpiryTimeStamp] = useState<Date>(() => {
+        return savedExpireTimeStamp ? new Date(savedExpireTimeStamp) : getTwoMinutesFromNow();
+    })
 
     const timerRef = useRef<TimerRef>(null);
 
     const handleExpire = () => {
-        sendMessage('1499174122', 'Reminder: Your timer has finished. Please check the app.')
+        sendMessage('1499174122', 'Reminder: Your timer has finished. Please check the app.');
+        localStorage.removeItem('expiryTimeStamp');
     };
+
+    const handleTimerChange = (newExpiryTimeStamp: Date) => {
+        setExpiryTimeStamp(newExpiryTimeStamp);
+        localStorage.setItem('expiryTimeStamp', newExpiryTimeStamp.toString())
+    }
+
+    useEffect(() => {
+        if (savedExpireTimeStamp) {
+            timerRef.current?.restart(new Date(savedExpireTimeStamp));
+        }
+    }, []);
 
     return (
         <div className='flex flex-col justify-center items-center gap-2'>
@@ -35,7 +54,8 @@ export const Reminder: React.FC = () => {
                     <p className='font-medium text-sm'>Hamster</p>
                     <img src={hamster} width={25} height={25} alt='hamster-coin'/>
                 </div>
-                <Timer className='my-8' size='tiny' ref={timerRef} expiryTimestamp={getTwoMinutesFromNow()} onExpire={handleExpire}/>
+                <Timer className='my-8' size='tiny' ref={timerRef} expiryTimestamp={expiryTimeStamp}
+                       onExpire={handleExpire}  onTimerChange={handleTimerChange}/>
             </div>
         </div>
     )
