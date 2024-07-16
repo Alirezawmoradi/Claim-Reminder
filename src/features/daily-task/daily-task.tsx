23cc65980f74cd3663d23234c621cf859d09f637
Alirezaw <Alirezawmoradi@gmail.com>
2024-07-17 01:21:46 +0330

    daily task component created

---
+++ src/features/daily-task/daily-task.tsx
@@ -0,0 +1,28 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {Loading} from "../../components/loading/loading.tsx";
+import {Details} from "../../components/details/details.tsx";
+
+export const DailyTask = (activeItem: any) => {
+    const [data, setData] = useState<DailyTask | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+    const fetchData = async () => {
+        setLoading(true);
+        const result = await axios.get('https://nabikaz.github.io/HamsterKombat-API/config.json');
+        setData(result.data);
+        setLoading(false);
+    }
+    useEffect(() => {
+        fetchData();
+    }, [activeItem]);
+    return (
+        <div className='flex justify-center items-center w-screen h-screen '>
+            {
+                loading ?
+                    <Loading/>
+                    :
+                    <Details data={data}/>
+            }
+        </div>
+    )
+}
